11c27916b12236ed54552bdacb8c4ddf9a826d17
Vamnaone <vamnaone@gmail.com>
2025-08-12 07:07:08 +0700

    d

--- app/student/layout.js
+++ app/student/layout.js
@@ -1,11 +1,9 @@
-import Nav from "./nav";
 
 export default function Layout({ children }) {
     return (
         <>
             <div className="flex_col" style={{ height: '100%', width: '100%' }}>
-                <Nav />
-                <div style={{ flex: 1, marginTop: 12, overflow: 'hidden' }}>
+                <div style={{ flex: 1, overflow: 'hidden' }}>
                     {children}
                 </div>
             </div>
